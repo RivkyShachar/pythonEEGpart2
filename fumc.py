9ade6179f61c774ae79d48f8326e5dd71c8dc475
RivkyShachar <92647408+RivkyShachar@users.noreply.github.com>
2022-07-18 11:23:22 +0300

    read

--- fumc.py
+++ fumc.py
@@ -10,8 +10,14 @@ from numpy import std
     return list(map(lambda x: (x - mean(a)) / std(a), a))
 '''
 def normy(a):
-  b = (a-np.mean(a))/(np.std(a))
+  if np.std(a)==0.0:#if all the values are the same std==0
+    b=a-np.mean(a)
+  else:
+    b = (a-np.mean(a))/(np.std(a))
   return b
+
+
+
 import csv
 col_list=["AF3", "F7", "F3", "FC5", "T7", "P7", "O1", "O2", "P8", "T8", "FC6", "F4", "F8", "AF4"]
 filename = open('EEGLogger.csv', 'r')
