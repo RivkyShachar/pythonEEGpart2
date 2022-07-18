63169c43030fff3edd3152ac28ae859196602274
avigayilm <amandel@g.jct.ac.il>
2022-07-18 14:03:24 +0300

    solved ICA problem by increaasing the tol

--- fastica.py
+++ fastica.py
@@ -16,7 +16,7 @@ from sklearn.tree import DecisionTreeClassifier
 from matplotlib import pyplot
 
 def doFastICA(EEG_data):
-    ica = FastICA(n_components=14)
+    ica = FastICA(n_components=14,tol=0.1) # made the tolerance higher than the default in order that it converges.
     EEG_data = ica.fit_transform(EEG_data)  # Reconstruct signals
     mixing_matrix = ica.mixing_  # Get estimated mixing matrix
     return EEG_data
